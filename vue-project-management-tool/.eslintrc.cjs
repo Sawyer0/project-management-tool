191ef942e2b0e88980d9ca6d1b307101554a9eb7
Dawan Sawyer <dawansawyer@gmail.com>
2024-07-20 08:40:24 -0400

    setup: Setup Vue Router, Vuex, and Axios for FE develpopment Installed Vue Router for handling navigation Configured Vue Router with initial routes Installed and configured Vuex for state management Installed Axios for making HTTP requests

--- vue-project-management-tool/.eslintrc.cjs
+++ vue-project-management-tool/.eslintrc.cjs
@@ -3,23 +3,28 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
+  env: {
+    node: true,
+    es6: true,
+    browser: true
+  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    babelOptions: {
+      presets: ['@babel/preset-env']
+    }
+  },
   overrides: [
     {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/support/**/*.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended']
     }
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
+  rules: {
+    'prettier/prettier': 'error'
   }
 }
